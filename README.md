fb589199751f94926b913b409ea536e5de03d4dc
Pathomwong Suwanpanich <guzzlery@gmail.com>
2018-05-14 22:54:53 -0500

    Init Commit

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
+# Classic Arcade Game Clone
+
+Classic Arcade Game Clone is browser-based game for Front End Udacity Nanodegree
+
+## Table of Contents
+
+* [Installation of the Game](#installation)
+* [How to Play the Game](#howtoplay)
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+
+## <a name="installation">Installation of the Game
+  Clone This git repository
+  ```
+  git clone https://github.com/pathomwong/frontend-nanodegree-arcade-game.git
+  ```
+  This Game is a web-base game, Installation is not needed, just open index.html with your browser.
+
+## <a name="howtoplay"></a>How to Play the Game
+  In this game you have a Player and Enemies (Bugs). The goal of the player is to reach the water, without colliding into any one of the enemies. The player can move left, right, up and down(Using arrow on the keyboard). The enemies move in varying speeds on the paved block portion of the scene. Once a the player collides with an enemy, the game is reset and the player moves back to the start square. Once the player reaches the water the game is won.
+
+  [Source](https://classroom.udacity.com/nanodegrees/nd001/parts/c02fda3b-67bf-48d6-a64f-c6960e2d4d79/modules/269645859775463/lessons/2696458597239847/concepts/59a9fe1d-cab4-4256-8479-4550ce4f4cfd)
+
+## Instructions
+
 frontend-nanodegree-arcade-game
 ===============================
 
 Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
 
 For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
+const LEFT = 0;
+const	TOP = 20;
+const	RIGHT = 390;
+const	BOTTOM = 390;
+const ENEMY_START_Y = [60,145,225];
+const ENEMY_MAX_SPEED = 300;
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = 0;
+    this.y = this.genStartY();
+    this.speed = this.genSpeed();
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +24,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.checkCollisions();
+    this.x = this.x + (this.speed * dt);
+    if(this.x > (RIGHT+150)){
+      this.restart();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +36,112 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Check for collision
+Enemy.prototype.checkCollisions = function() {
+    if(player.x < this.x + 75 &&
+       player.x > this.x - 75 &&
+       player.y < this.y + 55 &&
+       player.y > this.y - 55
+    ){
+      player.reset();
+    }
+};
+
+//reuse enemy
+Enemy.prototype.restart = function() {
+    this.x = 0;
+};
+
+// Generate the random speen
+Enemy.prototype.genSpeed = function() {
+    return Math.floor(Math.random() * ENEMY_MAX_SPEED) + 50;
+};
+
+// Generate the random start point Y
+Enemy.prototype.genStartY = function() {
+    return ENEMY_START_Y[Math.floor(Math.random() * 3)];
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player{
 
+  constructor(){
+      this.sprite = 'images/char-boy.png';
+      this.x = 0;
+      this.y = 390;
+      this.goal = false;
+  }
+
+  update(){
+      this.getGoal();
+  }
+
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    if(this.goal){
+      ctx.font = "bolder 30px Arial";
+      ctx.fillStyle="#FF0000";
+      ctx.fillText("You Got It, Try Again!", 100, 100);
+    }
+  }
+
+  handleInput(keyPressed){
+      this.goal = false;
+      switch(keyPressed){
+        case 'left': this.moveLeft();break;
+        case 'right': this.moveRight();break;
+        case 'up': this.moveUp();break;
+        case 'down': this.moveDown();break;
+      }
+  }
+
+  moveLeft(){
+    if(this.x > LEFT){
+      this.x -= 102;
+    }
+  }
+
+  moveRight(){
+    if(this.x  < RIGHT){
+      this.x += 102;
+    }
+  }
+
+  moveUp(){
+    if(this.y > TOP){ // this cheching is no needed, but just in case
+      this.y -= 83;
+    }
+  }
+
+  moveDown(){
+    if(this.y < BOTTOM){
+      this.y += 83;
+    }
+  }
+
+  getGoal(){
+    if(this.y <= 0){
+      this.goal = true;
+      this.reset();
+    }
+  }
+
+  reset(){
+    this.x = 0;
+    this.y = 390;
+  }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+const allEnemies = [];
+const player = new Player();
+for(let i=0;i < 6;i++){
+  allEnemies.push(new Enemy());
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
